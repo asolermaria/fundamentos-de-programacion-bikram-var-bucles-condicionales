c113b3446d873c09796560ee377e9c66081c36f8
asolermaria <asolermaria@gmail.com>
2026-03-02 15:19:49 +0100

    Done ex 1,2,3

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1.- Crear variable tipo let de nombre variableSinValor declarada sin valor
+let variableSinValor;
+
+// 2.- Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos
+let booleano1= true;
+let booleano2= false;
+
+// 3.- Crear variable tipo const de nombre PI declarada con valor 3.14
+const PI = 3.14;
